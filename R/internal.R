fdea695517cf1f1330b009d30a0516ca5740a20e
Andreas Eckner <andreas@eckner.com>
2015-12-11 16:21:46 -0500

    Add lynx_1942 dataset

--- R/internal.R
+++ R/internal.R
@@ -6,7 +6,7 @@
 #'   \item \code{\link{download_co2_20ma}}
 #'   \item \code{\link{dowload_paris_precipitation}}
 #'   \item \code{\link{download_grapes}}
-#'   \item \code{\link{import_lynx}}
+#'   \item \code{\link{import_lynx_1942}}
 #' }
 #' 
 #' @name utsData-internal
--- R/lynx.R
+++ R/lynx.R
@@ -1,18 +1,18 @@
-#' Import Lynx Data
+#' Annual Canadian Lynx trappings 1821-1939
 #' 
-#' This function imports the lynx data from Elton and Nicholson (1942) from a local CSV file. It subsequently returns the data as as \code{\link{uts_vector}} object.
+#' The annual number of Canadian lynx (\emph{Lynx canadensis}) catches by the Hudson's Bay Company. Each recorded year (called \emph{Outfit}) counts collected furs from 1 June of that year until 31 May of the following year.
 #' 
-#' @keywords datasets
-#' @seealso The \code{\link[datasets:lynx]{lynx}} dataset in base \R is similar, except that it: \itemize{
-#'   \item contains data for only a single geographic region (instead of all eleven regions),
-#'   \item has an obvious typo (compared to its quoted source).
-#' }
+#' This function imports the lynx data from a local CSV file and returns it as as \code{\link{uts_vector}} object.
+#' 
+#' @return A \code{\link[utsMultivariate:uts_vector]{uts_vector}} object. An observation for date, let's say, 1840-05-31 represents the total number of lynx catches for outfit 1839, which ran from 1 June 1839 to 31 May 1840.
+#' @seealso The already imported data can be accessed via \code{\link{lynx_1942}}.
+#' @seealso The \code{\link[datasets:lynx]{lynx}} dataset in base \R is similar, except that it (i) contains data for only a single geographic region (instead of all eleven regions), (ii) has an obvious typo (compared to its quoted source).
 #' 
 #' @references Elton, C. and Nicholson, M. (1942). \href{http://www.jstor.org/stable/1358}{The Ten-Year Cycle in Numbers of the Lynx in Canada}. \emph{Journal of Animal Ecology}, Vol 11, No. 2, pp. 215-244
 #' 
 #' @keywords datasets internal
 #' @examples
-#' lynx_1942 <- import_lynx()
+#' lynx_1942 <- import_lynx_1942()
 #' plot(lynx_1942[["MacKenzie River"]])
 #' #plot(lynx_1942, max_dt=dyears(2), type="o", cex=0.5)    # not implemented yet
 #' 
@@ -20,15 +20,33 @@
 #' \dontrun{
 #'   save(lynx_1942, file=file.path("data", "lynx_1942.rda"), compress="xz")
 #' }
-import_lynx <- function()
+import_lynx_1942 <- function()
 {
   # Read data
   file <- system.file(file.path("exdata", "lynx_1942.csv"), package="utsData")
   data <- read.csv(file, check.names=FALSE)
   
   # Convert to "uts_vector"
-  times <- ISOdate(data$Year, month=1, day=1, hour=0)
+  times <- ISOdate(data$Year + 1, month=5, day=31, hour=0)
   out <- uts_vector_wide(data[,-1], times)
   na.omit(out)
 }
 
+
+#' Annual Canadian Lynx trappings 1821-1939
+#'
+#' The annual number of Canadian lynx (\emph{Lynx canadensis}) catches by the Hudson's Bay Company. Each recorded year (called \emph{Outfit}) counts collected furs from 1 June of that year until 31 May of the following year.
+#'
+#' @format A \code{\link[utsMultivariate:uts_vector]{uts_vector}} object. An observation for date, let's say, 1840-05-31 represents the total number of lynx catches for outfit 1839, which ran from 1 June 1839 to 31 May 1840.
+#'
+#' @source Elton, C. and Nicholson, M. (1942). \href{http://www.jstor.org/stable/1358}{The Ten-Year Cycle in Numbers of the Lynx in Canada}. \emph{Journal of Animal Ecology}, Vol 11, No. 2, pp. 215-244.
+#'
+#' @seealso \code{\link{import_lynx_1942}}
+#' @seealso The \code{\link[datasets:lynx]{lynx}} dataset in base \R is similar, except that it (i) contains data for only a single geographic region (instead of all eleven regions), (ii) has an obvious typo (compared to its quoted source).
+#' 
+#' @name lynx_1942
+#' @keywords datasets
+#' @examples
+#' data(lynx_1942)
+#' plot(lynx_1942[["MacKenzie River"]])
+NULL
