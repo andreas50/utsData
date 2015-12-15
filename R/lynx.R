e5f4f4ac67b12cae7bf9494be6922e9d97a83992
Andreas Eckner <andreas@eckner.com>
2015-12-15 11:30:04 -0500

    Remove redundant lynx import documentation

--- R/lynx.R
+++ R/lynx.R
@@ -1,22 +1,14 @@
 #' Annual Canadian Lynx trappings 1821-1939
 #' 
-#' The annual number of Canadian lynx (\emph{Lynx canadensis}) catches by the Hudson's Bay Company. Each recorded year (called \emph{Outfit}) counts collected furs from 1 June of that year until 31 May of the following year.
-#' 
-#' This function imports the lynx data from a local CSV file and returns it as as \code{\link{uts_vector}} object.
-#' 
-#' @return A \code{\link[utsMultivariate:uts_vector]{uts_vector}} object. An observation for date, let's say, 1840-05-31 represents the total number of lynx catches for outfit 1839, which ran from 1 June 1839 to 31 May 1840.
-#' @seealso The already imported data can be accessed via \code{\link{lynx_1942}}.
-#' @seealso The \code{\link[datasets:lynx]{lynx}} dataset in base \R is similar, except that it (i) contains data for only a single geographic region (instead of all eleven regions), (ii) has an obvious typo (compared to its quoted source).
-#' 
-#' @references Elton, C. and Nicholson, M. (1942). \href{http://www.jstor.org/stable/1358}{The Ten-Year Cycle in Numbers of the Lynx in Canada}. \emph{Journal of Animal Ecology}, Vol 11, No. 2, pp. 215-244
+#' This function generates the \code{\link{lynx_1942}} data by importing the observations from a local CSV file and returning them as as \code{\link{uts_vector}} object. It is not meant to be called directly, but provided for reproducability.
 #' 
 #' @keywords datasets internal
 #' @examples
-#' lynx_1942 <- import_lynx_1942()
-#' plot(lynx_1942[["MacKenzie River"]])
-#' #plot(lynx_1942, max_dt=dyears(2), type="o", cex=0.5)    # not implemented yet
+#' # Show the raw data
+#' file.show(system.file(file.path("exdata", "lynx_1942.csv"), package="utsData"))
 #' 
-#' # Save data
+#' # Import and save the data
+#' lynx_1942 <- import_lynx_1942()
 #' \dontrun{
 #'   save(lynx_1942, file=file.path("data", "lynx_1942.rda"), compress="xz")
 #' }
@@ -41,7 +33,6 @@ import_lynx_1942 <- function()
 #'
 #' @source Elton, C. and Nicholson, M. (1942). \href{http://www.jstor.org/stable/1358}{The Ten-Year Cycle in Numbers of the Lynx in Canada}. \emph{Journal of Animal Ecology}, Vol 11, No. 2, pp. 215-244.
 #'
-#' @seealso \code{\link{import_lynx_1942}}
 #' @seealso The \code{\link[datasets:lynx]{lynx}} dataset in base \R is similar, except that it (i) contains data for only a single geographic region (instead of all eleven regions), (ii) has an obvious typo (compared to its quoted source).
 #' 
 #' @name lynx_1942
