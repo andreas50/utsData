29bbd71b490542e93df17e7306ebca7d99eee4df
Andreas Eckner <andreas@eckner.com>
2015-12-18 09:33:40 -0500

    New development release

--- R/internal.R
+++ R/internal.R
@@ -1,7 +1,6 @@
 #' Internal Functions
 #'
-#' The internal functions listed below allow to reproduce the data download and import of the datasets provided by this package.
-#' 
+#' The following internal functions allow to reproduce the data download and import of the datasets provided by this package:
 #' \itemize{
 #'   \item \code{\link{download_co2_20ma}}
 #'   \item \code{\link{download_grapes}}
